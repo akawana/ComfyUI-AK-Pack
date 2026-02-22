ec99060b807ed3652967c971243c961e69c35637
akawana <pishite@protonmail.com>
2026-02-22 18:30:18 +0800

    f

--- nodes/IsOneOfGroupsActive.py
+++ nodes/IsOneOfGroupsActive.py
@@ -1,5 +1,6 @@
 import math
 
+
 class IsOneOfGroupsActive:
     @classmethod
     def INPUT_TYPES(cls):
@@ -18,7 +19,7 @@ class IsOneOfGroupsActive:
                         "default": False,
                     },
                 ),
-            }
+            },
         }
 
     RETURN_TYPES = ("BOOLEAN",)
@@ -31,7 +32,19 @@ class IsOneOfGroupsActive:
         return float("nan")
 
     def pass_state(self, group_name_contains, active_state):
-        return (active_state,)
+        # Backward compatible behavior:
+        # Historically this node was a simple pass-through for `active_state`.
+        #
+        # Extension:
+        # If some upstream logic provides `active_state` as a list/tuple/set (e.g. one boolean per group),
+        # then return True if at least one element is True.
+        if isinstance(active_state, dict):
+            return (any(bool(v) for v in active_state.values()),)
+
+        if isinstance(active_state, (list, tuple, set)):
+            return (any(bool(x) for x in active_state),)
+
+        return (bool(active_state),)
 
 
 NODE_CLASS_MAPPINGS = {
